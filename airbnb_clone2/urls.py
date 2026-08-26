a2392affd601bf0603770a5284ee4746f297508c
Lucas Warmling Jasper <warmlingjasper@gmail.com>
2026-08-26 18:35:30 +0100

    Fix media files in production

--- airbnb_clone2/urls.py
+++ airbnb_clone2/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
